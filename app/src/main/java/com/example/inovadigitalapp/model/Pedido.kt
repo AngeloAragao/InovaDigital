1f762dadeed909b63123617ab49b185738fa7405
DevLuanguedes <105018666+DevLuanguedes@users.noreply.github.com>
2025-04-08 16:48:08 -0300

    Luan

---
+++ app/src/main/java/com/example/inovadigitalapp/model/Pedido.kt
@@ -0,0 +1,16 @@
+package com.example.inovadigitalapp.model
+
+class Pedido {
+
+    var nomeCliente = ""
+    var tipoServico = ""
+    var descricaoPedido = ""
+    var quantidadePedido = ""
+    var entregaPedido = ""
+    var valorPedido = ""
+    override fun toString(): String {
+        return "Pedido(nomeCliente='$nomeCliente', tipoServico='$tipoServico', descricaoPedido='$descricaoPedido', quantidadePedido='$quantidadePedido', entregaPedido='$entregaPedido', valorPedido='$valorPedido')"
+    }
+
+
+}
